2adbabbd398abed143b7dec3c80aaae7e3a541f1
Mira <mid2qj@virginia.edu>
2018-03-01 17:38:34 -0500

    Added verbose parameter.

--- print_or_show_message.m
+++ print_or_show_message.m
@@ -41,6 +41,7 @@ function print_or_show_message(message, varargin)
 %   2018-02-08 AL - Reverted back to using uiwait() and 'modal'
 %   2018-02-26 MD - Modified to have messageMode and removed toShow, messagebox
 %                   always shows now with the fprintf being optional.
+%   2018-03-01 MD - Added verbose parameter
 
 %% Hard-coded parameters
 validMessageModes = {'wait', 'show', 'none'};
@@ -52,6 +53,8 @@ iconDefault = 'none';               % default : Does not display an icon with
                                     %   with message box.
 messageModeDefault = 'wait';        % default : Pauses program and displays
                                     %   message box.
+verboseDefault = false;           % default: Program does not print message
+                                    %   even if message box is shown
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -70,16 +73,25 @@ addParameter(iP, 'MTitle', mTitleDefault, @ischar);
 addParameter(iP, 'Icon', iconDefault, @ischar);
 addParameter(iP, 'MessageMode', messageModeDefault, ...
     @(x) any(validatestring(x, validMessageModes)));
+addParameter(iP, 'Verbose', verboseDefault, ...
+    @(x) validateattributes(x, {'logical', 'numeric'}, {'binary'}));
 
 % Read from the Input Parser
 parse(iP, message, varargin{:});
 mTitle = iP.Results.MTitle;
 icon = iP.Results.Icon;
+verbose = iP.Results.Verbose;
 
 % Match possibly ambiguous strings to valid strings
 messageMode = validatestring(iP.Results.MessageMode, validMessageModes);
 
-%   Display message box and/or stop program
+% Print to standard output if verbose is true or messageMode == 'none'
+if verbose || strcmp(messageMode, 'none')
+    messageStr = print_cellstr(message, 'Delimiter', '\n', 'OmitNewline', true);
+    fprintf('%s\n', messageStr);
+end
+
+% Display message box and/or stop program
 switch messageMode
 case 'wait'
     %   Program stops and displays message box               
@@ -88,10 +100,7 @@ case 'show'
     %   Program does not stop but still displays message box
     msgbox(message, mTitle, icon, 'modal');
 case 'none'
-    %   Program does not stop or display message box and prints message in
-    %   standard output
-    messageStr = print_cellstr(message, 'Delimiter', '\n', 'OmitNewline', true);
-    fprintf('%s\n', messageStr); 
+    %   Program does not stop or display message box
 otherwise
     error(['This is not a recognized message mode. ', ...
             'Valid message modes are ''wait'', ''show'', or ''none''.']);
@@ -101,6 +110,9 @@ end
 
 %{
 OLD CODE:
+
+addParameter(iP, 'Verbose', verboseDefault, ...
+    @(x) any(validatestring(x, {'true', 'false'})));
 
 addRequired(iP, 'toShow', ...               % whether to show message box
     @(x) validateattributes(x, {'logical', 'numeric'}, {'binary'}));
@@ -140,5 +152,18 @@ function print_or_show_message(toShow, message, mTitle, icon, varargin)
 if toShow == 1
 
     msgbox(message, mTitle, icon);
+
+if verbose && (strcmp(messageMode, 'wait') ||  strcmp(messageMode, 'show'))
+
+case 'none'
+    %   Program does not stop or display message box and prints message in
+    %   standard output
+    messageStr = print_cellstr(message, 'Delimiter', '\n', 'OmitNewline', true);
+    fprintf('%s\n', messageStr); 
+
+else
+% warning('off', verbose) isn't this how verbose is used? I also looked in
+% minEASE to see how it was used but I'm still confused. I was able to add it
+% as a pair parameter though!
 
 %}
