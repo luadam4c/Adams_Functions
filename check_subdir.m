3d6902b434e9d8dd0755b85fdf33328e48413669
adam <luadam4c@gmail.com>
2018-06-19 18:12:23 -0400

    Updated check_subdir.m

--- check_subdir.m
+++ check_subdir.m
@@ -1,24 +1,26 @@
-function check_subdir (outfolder, subdir)
-%% Checks if needed subdirectory(ies) exist in outfolder
-% Usage: check_subdir (outfolder, subdir)
+function check_subdir (parentDirectory, subDirectories)
+%% Checks if needed subdirectory(ies) exist in parentDirectory
+% Usage: check_subdir (parentDirectory, subDirectories)
 % Arguments:
-%		%%% TO DO
+%       %%% TO DO
 %
-% Used by:	
-%		/media/adamX/m3ha/data_dclamp/take4/find_special_cases.m
-%		/media/adamX/m3ha/optimizer4gabab/optimizer_4compgabab.m
-%		/home/Matlab/Adams_Functions/find_passive_params.m
-%		/home/Matlab/Adams_Functions/find_istart.m
-%		/home/Matlab/Adams_Functions/find_IPSC_peak.m
-%		/home/Matlab/Adams_Functions/find_LTS.m
-%		/home/Matlab/Adams_Functions/create_subdir_copy_files.m
-%		/home/Matlab/Adams_Functions/create_input_file.m
+% Used by:    
+%       /media/adamX/m3ha/data_dclamp/take4/find_special_cases.m
+%       /media/adamX/m3ha/optimizer4gabab/optimizer_4compgabab.m
+%       /media/adamX/m3ha/optimizer4gabab/singleneuronfitting22.m
+%       /home/Matlab/Adams_Functions/find_passive_params.m
+%       /home/Matlab/Adams_Functions/find_istart.m
+%       /home/Matlab/Adams_Functions/find_IPSC_peak.m
+%       /home/Matlab/Adams_Functions/find_LTS.m
+%       /home/Matlab/Adams_Functions/create_subdir_copy_files.m
+%       /home/Matlab/Adams_Functions/create_input_file.m
 %       /home/Matlab/minEASE/minEASE.m
 %       /home/Matlab/EEG_gui/EEG_gui.m
 %       /home/Matlab/function_template.m
 %
 % File History:
 % 2016-11-02 Created
+% 2018-06-19 Changed tabs to spaces
 % TODO: Use print_or_show_message
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -27,19 +29,19 @@ function check_subdir (outfolder, subdir)
 %%% TO DO
 
 %% Check subdirectory(ies)
-if iscell(subdir)
-	for k = 1:numel(subdir)
-		if exist(fullfile(outfolder, subdir{k}), 'dir') ~= 7
-			mkdir(fullfile(outfolder, subdir{k}));
-			fprintf('New subdirectory is made: %s\n\n', ...
-				fullfile(outfolder, subdir{k}));
-		end
-	end
+if iscell(subDirectories)
+    for k = 1:numel(subDirectories)
+        if exist(fullfile(parentDirectory, subDirectories{k}), 'dir') ~= 7
+            mkdir(fullfile(parentDirectory, subDirectories{k}));
+            fprintf('New subdirectory is made: %s\n\n', ...
+                fullfile(parentDirectory, subDirectories{k}));
+        end
+    end
 else
-	if exist(fullfile(outfolder, subdir), 'dir') ~= 7
-		mkdir(fullfile(outfolder, subdir));
-		fprintf('New subdirectory is made: %s\n\n', ...
-				fullfile(outfolder, subdir));
-	end
+    if exist(fullfile(parentDirectory, subDirectories), 'dir') ~= 7
+        mkdir(fullfile(parentDirectory, subDirectories));
+        fprintf('New subdirectory is made: %s\n\n', ...
+                fullfile(parentDirectory, subDirectories));
+    end
 end
 
