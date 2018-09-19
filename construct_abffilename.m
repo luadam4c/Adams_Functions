39176ef79bafdfdf578d5a99cc97b3f7f67c2b18
adam <luadam4c@gmail.com>
2018-09-19 11:10:44 -0400

    Updated documentation

--- construct_abffilename.m
+++ construct_abffilename.m
@@ -1,19 +1,20 @@
-function abffilename_full = construct_abffilename(filename)
+function abfFullFileName = construct_abffilename(filename)
 %% Constructs the full file name to an abf file robustly based on filename and display message if doesn't exist
-% Usage: abffilename_full = construct_abffilename(filename)
+% Usage: abfFullFileName = construct_abffilename(filename)
 % Arguments:
 %	filename	- can be either the full address or must be in current directory
 %				.abf is not needed (e.g. 'B20160908_0004')
 %
 % Used by:
-%		cd/plot_traces_abf.m
+%		cd/parse_abf.m
 %		cd/plot_FI.m
 % 
 % 2017-04-11 Moved from plot_traces_abf.m
-% TODO: use construct_fullfilename and rename this construct_and_check_fullfilename; make 'Extension' and optional parameter
+% TODO: use construct_fullfilename and rename this construct_and_check_fullfilename; 
+% TODO: make 'Extension' an optional parameter
 % 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Check arguments
 %%% TODO
@@ -24,9 +25,16 @@ abffilename = strcat(filebase, '.abf');
 if isempty(filepath)
 	filepath = pwd;
 end
-abffilename_full = fullfile(filepath, abffilename);
-fprintf('Full path to abf file: %s\n', abffilename_full);
-if exist(abffilename_full, 'file') ~= 2
-	error('This abf file doesn''t exist!');
+abfFullFileName = fullfile(filepath, abffilename);
+fprintf('Full path to abf file: %s\n', abfFullFileName);
+if exist(abfFullFileName, 'file') ~= 2
+	fprintf('This abf file doesn''t exist!');
+    return;
 end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%{
+OLD CODE:
+
+%}
