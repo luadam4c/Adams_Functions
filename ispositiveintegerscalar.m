4ab3c1a883bc0498d4e4a699434f7a5325164428
adam <luadam4c@gmail.com>
2018-12-10 00:24:39 -0500

    Update ispositiveintegerscalar.m

--- ispositiveintegerscalar.m
+++ ispositiveintegerscalar.m
@@ -20,6 +20,7 @@ function isPositiveIntegerScalar = ispositiveintegerscalar (x)
 % Used by:
 %       cd/compute_weighted_average.m
 %       cd/m3ha_plot_individual_tracesm
+%       ~/m3ha/optimizer4gabab/singleneuronfitting42.m and beyond
 
 % File History:
 % 2018-10-26 Adapted from ispositiveintegervector.m
