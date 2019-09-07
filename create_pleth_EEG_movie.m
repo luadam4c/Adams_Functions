93a842ceb0fd49343c195abcc6030cc7f5df9f04
adam <luadam4c@gmail.com>
2019-09-07 13:06:45 -0400

    Updated docs

--- create_pleth_EEG_movie.m
+++ create_pleth_EEG_movie.m
@@ -32,8 +32,9 @@
 
 % File History:
 % 2019-09-05 Created by Adam Lu
+% 2019-09-06 Added pleth and EMG channels
+% TODO: Add Light on info as raster
 % TODO: Use parse_spike2_mat.m without loading everything?
-% TODO: Add pleth and EMG channels
 
 %% Hard-coded parameters
 
